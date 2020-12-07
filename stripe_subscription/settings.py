96f677d2debe3aafcc9c6b763cd04a5f26fce2f1
Subhaac <subhaac@yahoo.co.uk>
2020-12-07 11:41:25 +0000

    Add localhost to allowed hosts in settings

--- stripe_subscription/settings.py
+++ stripe_subscription/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['stripe-sub.herokuapp.com']
+ALLOWED_HOSTS = ['stripe-sub.herokuapp.com', 'localhost']
 
 
 # Application definition
