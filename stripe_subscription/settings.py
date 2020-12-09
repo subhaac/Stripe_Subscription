75a11cc33a02f8a5af76a2580b098dbc5c520ba8
Subhaac <subhaac@yahoo.co.uk>
2020-12-09 14:58:50 +0000

    Change DB to Postgres

--- stripe_subscription/settings.py
+++ stripe_subscription/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'stripe_subscription.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stripe',
+        'USER': 'test_user_stripe',
+        'PASSWORD': 'km010a027',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
